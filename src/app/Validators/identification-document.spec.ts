0fb4527c497479869bac06c93ff296ed99710e44
fjcolmenartic <105924033+fjcolmenartic@users.noreply.github.com>
2022-07-14 10:50:50 +0200

    Create identification-document.spec.ts

---
+++ src/app/Validators/identification-document.spec.ts
@@ -0,0 +1,7 @@
+import { IdentificationDocument } from './identification-document';
+
+describe('IdentificationDocument', () => {
+  it('should create an instance', () => {
+    expect(new IdentificationDocument()).toBeTruthy();
+  });
+});
